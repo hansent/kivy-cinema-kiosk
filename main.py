e1444e1dd58e9ba7cefe0b866acd7c6feda5cfd1
Thomas Hansen <thomas.hansen@gmail.com>
2011-02-24 05:53:39 -0600

    trying to change video using properties

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ kivy.require('1.0.4')
 
 from kivy.app import App
 from kivy.core.window import Window
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, StringProperty
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.video import Video
@@ -18,14 +18,17 @@ movies = shelve.open('movie.shelve')
 
 
 class InfoScreen(FloatLayout):
+    title = StringProperty("Arthur")
+    trailer = StringProperty("trailers/arthur-tlr1_h720p.mov")
     progress = NumericProperty(0.0)
     def on_progress(self, instance, value):
         print value
         if value > 0.1:
-
+            
             m = random.choice(movies.values())
-            self.title = m.title
-            self.trailer = m.trailer
+            instance.title = m.title
+            instance.trailer = m.trailer
+            instance.progress = 0.0
 
 
 
