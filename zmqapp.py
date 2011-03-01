8f770a736df6903b5edb31705cb6447fe57918c9
Thomas Hansen <thomas.hansen@gmail.com>
2011-03-01 12:52:28 -0600

    add license information

--- zmqapp.py
+++ zmqapp.py
@@ -1,3 +1,21 @@
+#This file is part of the Kivy Cinema Kiosk Demo.
+#    Copyright (C) 2010 by 
+#    Thomas Hansen  <thomas@kivy.org>
+#    Mathieu Virbel <mat@kivy.org>
+#
+#    The Kivy Cinema Kiosk Demo is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    The Kivy Cinema Kiosk Demo is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with The Kivy Cinema Kiosk Demo.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import kivy, zmq, inspect
 kivy.require('1.0.4')
